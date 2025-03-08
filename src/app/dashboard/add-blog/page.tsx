e5adea9880b60e9b43ac7e22cdabe9ab05a2bb61
Raunak <raunakdob@gmail.com>
2025-03-09 00:10:14 +0530

    Comment out AddBlogForm component in AddBlogPage for future reference

--- src/app/dashboard/add-blog/page.tsx
+++ src/app/dashboard/add-blog/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import DashboardSidebar from "@/components/dashboard/DashboardSidebar"
-import AddBlogForm from "@/components/dashboard/AddBlogForm"
+// import AddBlogForm from "@/components/dashboard/AddBlogForm"
 
 function AddBlogPage() {
   return (
@@ -8,7 +8,7 @@ function AddBlogPage() {
       <DashboardSidebar />
       <main className="flex-1 p-6">
         <h1 className="text-3xl font-bold text-sky-800 mb-6">Add New Blog Post</h1>
-        <AddBlogForm />
+        {/* <AddBlogForm /> */}
       </main>
     </div>
   )
